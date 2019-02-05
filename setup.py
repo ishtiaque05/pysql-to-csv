8c468bb1419bf28159ea1880f62aa1cab52235e1
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-02-05 10:51:38 +0600

    Init Sql to CSV cli tools

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!usr/bin/python
+from setuptools import setup, find_packages
+setup(
+		name='sql-to-csv',
+		description='An application for converting sql to csv',
+		version='0.10',
+		packages=find_packages(),
+		python_requires='>=3.6',
+		test_suite="tests",
+		entry_points={
+			'console_scripts': [
+				'pysql-to-csv=pysql_to_csv.__main__:main'
+			]
+		}
+	)
+
+
